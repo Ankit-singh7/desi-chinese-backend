80d1650f2058a861ced19d568598f36fea18c661
Ankit-singh7 <ankit.as2307@gmail.com>
2021-03-27 00:30:00 +0530

    up

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.use(globalErrorMiddleware.globalNotFoundHandler);
 const server = http.createServer(app);
 // start listening to http server
 console.log(appConfig);
-server.listen(appConfig.port);
+server.listen(process.env.PORT || appConfig.port);
 server.on('error', onError);
 server.on('listening', onListening);
 
