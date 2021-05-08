6d5e1585e39e23bbb54faf1539fe6df231d68f41
Ankit-singh7 <ankit.as2307@gmail.com>
2021-05-09 03:25:15 +0530

    update

---
+++ models/AdminModel.js
@@ -0,0 +1,44 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let adminSchema = new Schema(
+ {
+ adminId: {
+   type: String,
+   default: '',
+   index: true,
+   unique: true
+ },
+ firstName: {
+   type: String,
+   default: ''
+ },
+ lastName: {
+   type: String,
+   default: ''
+ },
+ password: {
+   type: String,
+   default: ''
+ },
+ email: {
+   type: String,
+   default: ''
+ },
+
+ validationToken: { //will generate automatically while resetting password
+   type: String,
+   default: ''
+ },
+
+ createdOn :{
+   type:Date,
+   default:""
+ }
+
+ 
+})
+
+
+mongoose.model('Admin', adminSchema);
