54e945d99f473240baf9a365cdc7e5e4f3b93ca0
Ankit-singh7 <ankit.as2307@gmail.com>
2021-05-11 02:14:34 +0530

    update

--- routes/ingredientReport.js
+++ routes/ingredientReport.js
@@ -6,7 +6,7 @@ const auth = require('./../middlewares/auth')
 
 module.exports.setRouter = (app) => {
 
-    let baseUrl = `${appConfig.apiVersion}/ingredient-report`;
+    let baseUrl = `${appConfig.apiVersion}/report`;
 
     app.get(`${baseUrl}/view/all`, ingredientReportController.getAllIngredientReport);
 
