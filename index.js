e2b1e570c87bcc312ffc0f535119c4cf4a1de8a8
Ankit-singh7 <ankit.as2307@gmail.com>
2021-05-16 13:05:50 +0530

    uo

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 
 
-cron.schedule('59 12 * * *', function() {
+cron.schedule('10 1 * * *', function() {
   console.log('running a task every minute');
   sessionModel.find({session_status:'true'}).exec((err,result) => {
     if(err) {
