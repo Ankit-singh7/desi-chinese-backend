dcce2bb8b9aff9c82a817d41fdd92f53c7463eda
Ankit-singh7 <ankit.as2307@gmail.com>
2025-02-24 20:07:18 +0530

    added expense list and update session

---
+++ models/employeeExpenseModel.js
@@ -0,0 +1,39 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let employeeExpenseSchema = new Schema(
+ {
+  expense_id: {
+   type: String,
+   default: '',
+   index: true
+ },
+  employee_id: {
+   type: String,
+   default: '',
+ },
+ employee_name: {
+   type: String,
+   default: ''
+ },
+ expenses : [
+  {
+    expense_reason: {
+      type: String,
+      default: ''
+    },
+    expense_amount: {
+      type: String,
+      default: ''
+    }
+  }
+],
+ createdOn :{
+   type:Date,
+   default:""
+ }
+})
+
+
+mongoose.model('employeeExpense', employeeExpenseSchema);
